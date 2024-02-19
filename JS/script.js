7aa3c55f5ef8460a7eb52be21230dab151e46f58
msifatsadiq <msifatsadiq@gmail.com>
2024-02-19 11:21:30 +0600

    adding the scroll function in the buy ticket button

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+document.getElementById('nav-button').addEventListener('click', function () {
+    // console.log('clicked');
+    let phSection = document.getElementById('ph-section');
+    phSection.scrollIntoView();
+
+
+})
